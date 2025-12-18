86268ecca304a4296934dd2e5c257e2efcd327c0
ycFreddy <fredc@netc.fr>
2025-12-18 05:13:17 +0100

    Init

--- mistralme.bat
+++ mistralme.bat
@@ -1,10 +1,10 @@
 @echo off
+chcp 65001 >nul
 echo ---
 .\python-3.12.9-amd64\python.exe app\run.py
 if %errorlevel% neq 0 (
     echo Erreur lors de l'exécution
 )
-chcp 65001 >nul
 echo ---
 echo Fermer la fenêtre pour Quitter
 pause >nul
